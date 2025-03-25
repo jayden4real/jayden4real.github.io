c9a8ad78e2a98830b2650791b9e62ab62b4632c3
jayden4real <167343276+jayden4real@users.noreply.github.com>
2025-03-24 23:35:46 -0400

    Add files via upload

--- src/objects/Exit/Exit.js
+++ src/objects/Exit/Exit.js
@@ -1,1 +1,27 @@
+import { events } from "../../Events.js";
+import { GameObject } from "../../GameObject.js";
+import { resources } from "../../Resource.js";
+import { Sprite } from "../../Sprite.js";
+import { Vector2 } from "../../Vector2.js";
 
+export class Exit extends GameObject {
+    constructor(x, y) {
+        super({
+            position: new Vector2(x, y)
+        });
+        this.addChild(new Sprite({
+            resource: resources.images.exit
+        }))
+    }
+
+    ready() {
+        events.on("HERO_POSITION", this, pos => {
+            // detect overlap...
+            const roundedHeroX = Math.round(pos.x);
+            const roundedHeroY = Math.round(pos.y);
+            if(roundedHeroX === this.position.x && roundedHeroY === this.position.y) {
+                events.emit("HERO_EXITS")
+            }
+        })
+    }
+}
