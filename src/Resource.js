1e60fae2aed9db686903c34d69e53059d377dff4
jayden4real <167343276+jayden4real@users.noreply.github.com>
2025-03-24 23:38:59 -0400

    Add files via upload

---
+++ src/Resource.js
@@ -0,0 +1,38 @@
+class Resources {
+    constructor() {
+        // Everything we plan to download
+        this.toLoad = {
+            // Outdoor
+            sky: "public/sprites/Sky.png",
+            ground: "public/sprites/Ground.png",
+            tree: "public/sprites/Tree.png",
+            // Hero
+            hero: "public/sprites/Hero-Sheet.png",
+            shadow: "public/sprites/Shadow.png",
+            rod: "public/sprites/Rod.png",
+            // Cave
+            exit: "public/sprites/Exit.png",
+            cave: "public/sprites/Cave.png",
+            caveGround: "public/sprites/Cave-Ground.png",
+        };
+
+        // A bucket to keep all of our images
+        this.images = {};
+
+        // Load each image
+        Object.keys(this.toLoad).forEach(key => {
+            const img = new Image();
+            img.src = this.toLoad[key];
+            this.images[key] = {
+                image: img,
+                isLoaded: false
+            }
+            img.onload = () => {
+                this.images[key].isLoaded = true;
+            }
+        })
+    }
+};
+
+// Create one instance for the whole app to use
+export const resources = new Resources();
